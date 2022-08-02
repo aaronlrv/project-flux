36ef153e985f7d08fb30cef45fcc14dc7ab5d85c
aaronlrv <aaronvitharanage@gmail.com>
2022-08-02 19:48:39 +0400

    Style hero text

--- demo/src/components/Hero.js
+++ demo/src/components/Hero.js
@@ -1,7 +1,14 @@
 function Hero() {
   return (
-    <div class="bg-indigo-600 h-3/4">
-      <p> Hi</p>
+    <div class="bg-indigo-600 h-3/4 flex justify-center bg-gradient-to-b from-green-200 to-emerald-300">
+      <div class="h-full flex flex-col justify-center">
+        <p class="font-comforter text-[10rem] pb-3"> Website Debugger</p>
+        <div class="flex justify-center">
+          <p class="font-mouse font-light text-4xl">
+            Where all your web design issues are solved
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
