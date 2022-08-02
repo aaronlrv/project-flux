65c43978b9d3cfb0fcaf63d145e4f8f7be8a4a29
aaronlrv <aaronvitharanage@gmail.com>
2022-08-02 17:11:41 +0400

    Initialize react + tailwind

---
+++ demo/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 class="bg-orange-500"> hi</h1>
+    </div>
+  );
+}
+
+export default App;
