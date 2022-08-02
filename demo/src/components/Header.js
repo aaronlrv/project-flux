ac8b9b9f6b9c0415578626746026f55c9f960f96
aaronlrv <aaronvitharanage@gmail.com>
2022-08-02 19:48:28 +0400

    Style header

--- demo/src/components/Header.js
+++ demo/src/components/Header.js
@@ -2,9 +2,11 @@ function Header() {
   return (
     <div
       className="header"
-      class="h-12 flex justify-center items-center  bg-green-300"
+      class="h-12 flex justify-center items-center bg-green-400"
     >
-      <p class="flex justify-center"> Project Flux</p>
+      <p class="flex justify-center text-white font-mouse text-3xl tracking-widest">
+        Project Flux
+      </p>
     </div>
   );
 }
