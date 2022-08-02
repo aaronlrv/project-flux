ae4bc472be50515a67ebb13f185fb431a5fa117e
aaronlrv <aaronvitharanage@gmail.com>
2022-08-02 17:31:45 +0400

    Add header

---
+++ demo/src/components/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <div
+      className="header"
+      class="h-12 flex justify-center items-center  bg-green-300"
+    >
+      <p class="flex justify-center"> Project Flux</p>
+    </div>
+  );
+}
+
+export default Header;
