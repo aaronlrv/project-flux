4fb1b7c3316a862f31ddefc10bf8abc1fd69e907
aaronlrv <aaronvitharanage@gmail.com>
2022-08-02 21:53:29 +0400

    Final commit

--- demo/src/App.js
+++ demo/src/App.js
@@ -2,12 +2,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Footer from "./components/Footer";
+import Content from "./components/Content";
 
 function App() {
   return (
     <div className="App" class="h-screen">
       <Header />
       <Hero />
+      <Content />
+      <Footer />
     </div>
   );
 }
--- demo/tailwind.config.js
+++ demo/tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        comforter: ["Comforter", "cursive"],
+        mouse: ["Mouse Memoirs", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
